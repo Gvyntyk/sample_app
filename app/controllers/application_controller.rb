a590857c2858c206c2bfc210d866b9b22c854ad1
Gvyntyk <gvyntyk@gmail.com>
2015-01-26 01:07:25 +0200

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # include ApplicationHelper # http://stackoverflow.com/questions/23630949/undefined-method-full-title
 end
