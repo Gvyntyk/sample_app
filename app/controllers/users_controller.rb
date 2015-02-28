daeb33371e60e4f2989391bdc37cf40af41481e0
Gvyntyk <gvyntyk@gmail.com>
2015-02-28 23:06:47 +0200

    before first signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #
+      flash[:success] = "Welcome to the Simple App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -25,8 +26,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
+                                   :password_confirmation)
     end
 
-  
 end
