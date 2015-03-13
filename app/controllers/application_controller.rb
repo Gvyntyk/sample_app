54b0d38b585b24d090cd290e9365b97c13ac92c0
Gvyntyk <gvyntyk@gmail.com>
2015-03-13 16:21:12 +0200

    Before cucumber

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # include ApplicationHelper # http://stackoverflow.com/questions/23630949/undefined-method-full-title
+  include SessionsHelper
 end
