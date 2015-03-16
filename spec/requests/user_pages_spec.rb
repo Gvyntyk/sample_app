60d336949a6a81c5a013b98e2e563d375ba7710b
Gvyntyk <gvyntyk@gmail.com>
2015-03-16 21:57:28 +0200

    Before 9.3.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,24 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
+    end
+  end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -22,10 +40,10 @@ describe "User pages" do
         before { visit signup_path }
         let(:submit) { "Create my account" }
 
-        describe "with invalid information" do
+      describe "with invalid information" do
           it "should not create a user" do
           expect { click_button submit }.not_to change(User, :count)
-      end
+        end
 
         describe "after submission" do
           before { click_button submit }
@@ -67,7 +85,7 @@ describe "User pages" do
       sign_in user
       visit edit_user_path(user)
     end
-    
+
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
